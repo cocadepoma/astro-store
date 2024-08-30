4edd06ff4390beb5b2f195e4cfe673509429b16d
Paco <pacors88@gmail.com>
2024-08-30 07:29:01 +0200

    feature: add raw query for getting products with images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import auth from "auth-astro";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [tailwind(), db(), auth()],
+  integrations: [tailwind(), db(), auth(), react()],
   experimental: {
     actions: true
   }
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 import { loginUser, logout, registerUser } from './auth';
+import { getProductsByPage } from './products/get-products-by-page.action';
 
 export const server = {
   // actions
@@ -7,4 +8,6 @@ export const server = {
   loginUser,
   logout,
   registerUser,
+
+  getProductsByPage,
 };
